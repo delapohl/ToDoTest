f4f10b2575b95b5cdf3f9b07fb8cbeb22dbf8a63
Michael Pohl <github@pohlmail.com>
2022-01-01 21:19:40 -0500

    Update Mongo connection string

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.j4c6s.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 
 mongoose
     .connect(uri)
